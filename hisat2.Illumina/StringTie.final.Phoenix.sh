7f4a782fd8e5310c13b36e121fd7773e27b21e29
Mark Corbett <37854406+speleonut@users.noreply.github.com>
2020-08-31 18:33:13 +0930

    update to suit new file paths in Phoenix

--- hisat2.Illumina/StringTie.final.Phoenix.sh
+++ hisat2.Illumina/StringTie.final.Phoenix.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#SBATCH -p batch            	                            # partition (this is the queue your job will be added to) 
+#SBATCH -J st-final
+#SBATCH -o /hpcfs/users/%u/log/stringtie-final-slurm-%j.out
+#SBATCH -A robinson
+#SBATCH -p batch            	                                # partition (this is the queue your job will be added to) 
 #SBATCH -N 1               	                                # number of nodes (due to the nature of sequential processing, here uses single node)
 #SBATCH -n 8              	                                # number of cores (here uses 8)
 #SBATCH --time=06:00:00    	                                # time allocation, which has the format (D-HH:MM), here set to 30 min
@@ -7,12 +10,14 @@
 
 # Notification configuration 
 #SBATCH --mail-type=END					    # Type of email notifications will be sent (here set to END, which means an email will be sent when the job is done)
-#SBATCH --mail-type=FAIL   					# Type of email notifications will be sent (here set to FAIL, which means an email will be sent when the job is fail to complete)
-#SBATCH --mail-user=mark.corbett@adelaide.edu.au  	# CHANGE this if you aren't me! Email to which notification will be sent
+#SBATCH --mail-type=FAIL                                    # Type of email notifications will be sent (here set to FAIL, which means an email will be sent when the job is fail to complete)
+#SBATCH --mail-user=%u@adelaide.edu.au                      # Email to which notification will be sent
 
 #Set some paths
-HISAT2_INDEXES=/data/neurogenetics/RefSeq/HISAT2
-DefaultOutDir=$FASTDIR/BAM/RNASeq # You might change this depending on how you set up your \$FASTDIR
+HISAT2_INDEXES=/hpcfs/groups/phoenix-hpc-neurogenetics/RefSeq/HISAT2
+stringtiePath=/hpcfs/groups/phoenix-hpc-neurogenetics/executables/stringtie_latest
+userDir=/hpcfs/users/${USER}
+DefaultOutDir=${userDir}/BAM/RNASeq # You might change this depending on how you set up your \$FASTDIR
 
 # StringTie.final.Phoenix.sh
 usage()
@@ -26,14 +31,15 @@ echo "# StringTie.final.Phoenix.sh slurm submission script. Generates read count
 #
 # Options: 
 # -f	REQUIRED. Path and file name of a text file with sample ID, you can use your HISAT2 input file listed in the form \"sampleID path/to/read_1-1,...,path/to/read_n-1 /path/to/read_1-2,...,/path/to/read_n-2\"
-# -m	OPTIONAL (with caveats). Merged GTF from the previous stringtie --merge.  Will look in \$OutDir for the file with defailt name mergedOutput.gtf
+# -m	OPTIONAL (with caveats). Merged GTF from the previous stringtie --merge.  Will look in \$outDir for the file with defailt name mergedOutput.gtf
 # -o	OPTIONAL. Path to where you want to find your files. Default is $DefaultOutDir
 # -h | --help	Prints the message you are reading.
 #
 # History: 
 # Script created by: Mark Corbett on 10/07/2017
-# email: mark.corbett@adelaide.edu.au
-# Modified (Date, Name, Description):
+# email: mark dot corbett is at adelaide.edu.au
+# Modified (Date; Name; Description):
+# 31/08/2020; Mark Corbett; Update for new file paths on Phoenix
 #
 " 
 }
@@ -48,7 +54,7 @@ while [ "$1" != "" ]; do
                 mergedGTF=$1
                 ;;
         -o )    shift
-                OutDir=$1
+                outDir=$1
                 ;;
         -h | --help )    usage
                          exit 0
@@ -66,33 +72,30 @@ if [ -z "$SeqFile" ]; then #If sequence file list in a text file is not supplied
 	exit 1
 fi
 
-if [ -z "$OutDir" ]; then # If output directory not specified then use the default.
-    OutDir=$DefaultOutDir
-	echo "Using $OutDir as the working directory"
+if [ -z "$outDir" ]; then # If output directory not specified then use the default.
+    outDir=$DefaultOutDir
+    echo "Using $outDir as the working directory"
 fi
 
-if [ ! -d "$OutDir" ]; then
-		mkdir -p $OutDir
+if [ ! -d "$outDir" ]; then
+    mkdir -p $outDir
 fi
 
 if [ -z "$mergedGTF" ]; then # If merged gtf not specified then try to find it
-    if [ -s "$OutDir/mergedOutput.gtf" ]; then
-    	mergedGTF=$OutDir/mergedOutput.gtf
+    if [ -s "$outDir/mergedOutput.gtf" ]; then
+    	mergedGTF=$outDir/mergedOutput.gtf
     else
     	usage
-    	echo "#ERROR: You need to specify -m merged gtf file. I looked for it in $OutDir but I couldn't find it"
+    	echo "#ERROR: You need to specify -m merged gtf file. I looked for it in $outDir but I couldn't find it"
     	exit 1
     fi
 fi
 # Define files for the array
 sampleID=($(awk -F" " '{print $1}' $SeqFile))
 
-# Load modules
-module load StringTie/1.3.3-foss-2017a
-
 # Run final round of StringTie
-stringtie $OutDir/${sampleID[$SLURM_ARRAY_TASK_ID]}/${sampleID[$SLURM_ARRAY_TASK_ID]}.hisat2.bam \
+$stringtiePath/stringtie $outDir/${sampleID[$SLURM_ARRAY_TASK_ID]}/${sampleID[$SLURM_ARRAY_TASK_ID]}.hisat2.bam \
 -e -B -G $mergedGTF -p 8 \
--o $OutDir/${sampleID[$SLURM_ARRAY_TASK_ID]}/${sampleID[$SLURM_ARRAY_TASK_ID]}.stringtie.forBallgown.gtf \
--A $OutDir/${sampleID[$SLURM_ARRAY_TASK_ID]}/${sampleID[$SLURM_ARRAY_TASK_ID]}.stringtie.run2.gene_abund.txt \
--C $OutDir/${sampleID[$SLURM_ARRAY_TASK_ID]}/${sampleID[$SLURM_ARRAY_TASK_ID]}.stringtie.run2.cov_refs.gtf \
+-o $outDir/${sampleID[$SLURM_ARRAY_TASK_ID]}/${sampleID[$SLURM_ARRAY_TASK_ID]}.stringtie.forBallgown.gtf \
+-A $outDir/${sampleID[$SLURM_ARRAY_TASK_ID]}/${sampleID[$SLURM_ARRAY_TASK_ID]}.stringtie.run2.gene_abund.txt \
+-C $outDir/${sampleID[$SLURM_ARRAY_TASK_ID]}/${sampleID[$SLURM_ARRAY_TASK_ID]}.stringtie.run2.cov_refs.gtf \
--- hisat2.Illumina/StringTieMerge.Phoenix.sh
+++ hisat2.Illumina/StringTieMerge.Phoenix.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#SBATCH -J st-merge
+#SBATCH -o /hpcfs/users/%u/log/stringtie-merge-slurm-%j.out
+#SBATCH -A robinson
 #SBATCH -p batch            	                            # partition (this is the queue your job will be added to) 
 #SBATCH -N 1               	                                # number of nodes (due to the nature of sequential processing, here uses single node)
 #SBATCH -n 8              	                                # number of cores (here uses 8)
@@ -8,11 +11,13 @@
 # Notification configuration 
 #SBATCH --mail-type=END					    # Type of email notifications will be sent (here set to END, which means an email will be sent when the job is done)
 #SBATCH --mail-type=FAIL   					# Type of email notifications will be sent (here set to FAIL, which means an email will be sent when the job is fail to complete)
-#SBATCH --mail-user=mark.corbett@adelaide.edu.au  	# CHANGE this if you aren't me! Email to which notification will be sent
+#SBATCH --mail-user=%u@adelaide.edu.au  	# CHANGE this if you aren't me! Email to which notification will be sent
 
 #Set some paths
-HISAT2_INDEXES=/data/neurogenetics/RefSeq/HISAT2
-DefaultOutDir=$FASTDIR/BAM/RNASeq # You might change this depending on how you set up your \$FASTDIR
+HISAT2_INDEXES=/hpcfs/groups/phoenix-hpc-neurogenetics/RefSeq/HISAT2
+stringtiePath=/hpcfs/groups/phoenix-hpc-neurogenetics/executables/stringtie_latest
+userDir=/hpcfs/users/${USER}
+DefaultOutDir=${userDir}/BAM/RNASeq # You might change this depending on how you set up your \$FASTDIR
 
 # StringTieMerge.Phoenix.sh
 usage()
@@ -25,16 +30,17 @@ echo "# StringTieMerge.Phoenix.sh slurm submission script. Feed it a text file t
 #
 # Options: 
 # -f	OPTIONAL (with caveats). Path and file name of a text file with sequences listed in the form /path/to/out.1.gtf to final gtf with a newline character
-#       If not specified the script will try to find the files it needs; success is not guaranteed.
-# -g	REQUIRED. Genome index for hisat2. eg. grch38_tran
+#       If not specified the script will try to find the files it needs but success is not guaranteed.
+# -g	OPTIONAL. Genome index for hisat2. The default is grch38_tran
 # -p    OPTIONAL. Specific prefix for the merged gtf. Default is mergedOutput.gtf
-# -o	OPTIONAL. Path to where you want to find your files default is $FASTDIR/BAM/RNASeq
+# -o	OPTIONAL. Path to where you want to find your files default is $DefaultOutDir
 # -h | --help	Prints the message you are reading.
 #
 # History: 
 # Script created by: Mark Corbett on 12/07/2017
-# email: mark.corbett@adelaide.edu.au
-# Modified (Date, Name, Description):
+# email: mark dot corbett is at adelaide.edu.au
+# Modified (Date; Name; Description):
+# 31/08/2020; Mark Corbett; Update for new file paths on Phoenix
 #
 " 
 }
@@ -52,7 +58,7 @@ while [ "$1" != "" ]; do
                 gtfPrefix=$1
                 ;;
         -o )    shift
-                OutDir=$1
+                outDir=$1
                 ;;
         -h | --help )    usage
                          exit 0
@@ -66,11 +72,9 @@ done
 # Check that your script has everything it needs to start.
 
 
-if [ -z "$refSeq" ]; then # If genome not specified then do not proceed
-	usage
-	echo "#ERROR: You need to specify -g genome build
-	# -g	REQUIRED. Genome build your RNASeq is mapped to."
- 	exit 1
+if [ -z "$refSeq" ]; then # If genome not specified then default to grch38_tran
+        refSeq=grch38_tran
+		echo "# WARN: No genome build selected so defulting to $refSeq"
 fi
 
 if [ -z "$gtfPrefix" ]; then #If gtfPrefx not specified then use a generic one
@@ -78,35 +82,32 @@ if [ -z "$gtfPrefix" ]; then #If gtfPrefx not specified then use a generic one
 	echo "INFO: Using $gtfPrefix as the output file name"
 fi
 
-if [ -z "$OutDir" ]; then #If output directory not specified then run in default directory
-    OutDir=$DefaultOutDir
-	echo "INFO: Using $OutDir as the output directory"
+if [ -z "$outDir" ]; then #If output directory not specified then run in default directory
+    outDir=$DefaultOutDir
+	echo "INFO: Using $outDir as the output directory"
 fi
 
-if [ ! -d "$OutDir" ]; then
-		mkdir -p $OutDir
+if [ ! -d "$outDir" ]; then
+    mkdir -p $outDir
 fi
 
 if [ -z "$gtfList" ]; then #If a gtf file list text file is not supplied then try to find them or die after this horrible nested if sequence!
-	find $OutDir/*/*.stringtie.run1.gtf > $OutDir/tmp.gtfList.txt
-	if [ -s $OutDir/tmp.gtfList.txt ]; then
-		gtfList=$OutDir/tmp.gtfList.txt
+	find $outDir/*/*.stringtie.run1.gtf > $outDir/tmp.gtfList.txt
+	if [ -s "$outDir/tmp.gtfList.txt" ]; then
+		gtfList=$outDir/tmp.gtfList.txt
 	else
-	    find $DefaultOutDir/*/*.stringtie.run1.gtf > $OutDir/tmp.gtfList.txt
-	    if [ -s "$OutDir/tmp.gtfList.txt" ]; then
-	    	gtfList=$OutDir/tmp.gtfList.txt
+	    find $DefaultoutDir/*/*.stringtie.run1.gtf > $outDir/tmp.gtfList.txt
+	    if [ -s "$outDir/tmp.gtfList.txt" ]; then
+	    	gtfList=$outDir/tmp.gtfList.txt
 	    else
             usage
-            echo "#ERROR: You need to specify the path and name of the GTF file list I looked in subfolders of $OutDir and $DefaultOutDir to create the file but the GTF can't be located"
+            echo "#ERROR: You need to specify the path and name of the GTF file list I looked in subfolders of $outDir and $DefaultoutDir to create the file but the GTF can't be located"
             exit 1
         fi
     fi
 fi
 
-# Load the modules
-module load StringTie/1.3.3-foss-2017a
-
 # Run the script
-stringtie --merge -p8 -G $HISAT2_INDEXES/$refSeq/$refSeq.gff \
--o $OutDir/$gtfPrefix \
+$stringtiePath/stringtie --merge -p8 -G $HISAT2_INDEXES/$refSeq/$refSeq.gff \
+-o $outDir/$gtfPrefix \
 $gtfList
