b1c50fb3d9b2fa7567b2b3b85afa9833bfacf26a
Mark Corbett <37854406+speleonut@users.noreply.github.com>
2023-06-29 12:23:26 +0930

    reverse calls to python and samtools modules

--- hisat2.Illumina/prepDE.sh
+++ hisat2.Illumina/prepDE.sh
@@ -18,7 +18,7 @@ stringtiePath=/hpcfs/groups/phoenix-hpc-neurogenetics/executables/stringtie_late
 userDir=/hpcfs/users/${USER}
 module purge
 module use /apps/skl/modules/all
-modList=("HTSlib/1.17-GCC-11.2.0" "Python/3.7.0")
+modList=("Python/3.7.0" "HTSlib/1.17-GCC-11.2.0")
 
 usage()
 {
